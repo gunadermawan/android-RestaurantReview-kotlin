64c649d0684be91e05e57ca6108b4d6f8a4f01ff
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-10-10 15:46:01 +0700

    melengkapi kode ApiService

--- app/src/main/java/com/gunder/restaurantreview/ApiService.kt
+++ app/src/main/java/com/gunder/restaurantreview/ApiService.kt
@@ -1,4 +1,11 @@
 package com.gunder.restaurantreview
 
-class ApiService {
+import retrofit2.Call
+import retrofit2.http.*
+
+interface ApiService {
+    @GET("detail/{id}")
+    fun getRestaurant(
+        @Path("id") id: String
+    ): Call<RestaurantResponse>
 }
