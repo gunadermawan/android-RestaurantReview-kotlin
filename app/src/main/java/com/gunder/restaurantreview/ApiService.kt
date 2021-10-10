69e4ddddc06f95bb763294a5eed0decc4530b85b
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-10-10 20:09:09 +0700

    add fun to send data in ApiService.kt

--- app/src/main/java/com/gunder/restaurantreview/ApiService.kt
+++ app/src/main/java/com/gunder/restaurantreview/ApiService.kt
@@ -8,4 +8,14 @@ interface ApiService {
     fun getRestaurant(
         @Path("id") id: String
     ): Call<RestaurantResponse>
+
+    //    untuk mengirim data
+    @FormUrlEncoded
+    @Headers("Authorization: token 12345")
+    @POST("REVIEW")
+    fun postReview(
+        @Field("id") id: String,
+        @Field("name") name: String,
+        @Field("review") review: String
+    ): Call<PostReviewResponse>
 }
