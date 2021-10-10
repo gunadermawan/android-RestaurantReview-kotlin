e60d4e938aab087b7be2643dee4134958bcf84a2
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-10-10 16:39:39 +0700

    add kode untuk ngambil data dg retrofit

--- app/src/main/java/com/gunder/restaurantreview/MainActivity.kt
+++ app/src/main/java/com/gunder/restaurantreview/MainActivity.kt
@@ -2,10 +2,95 @@ package com.gunder.restaurantreview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.gunder.restaurantreview.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Response
+import javax.security.auth.callback.Callback
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val TAG = "MainActivity"
+        private const val RESTAURANT_ID = "uewq1zg2zlskfw1e867"
+    }
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.rvView.layoutManager = layoutManager
+        val itemDecoration = DividerItemDecoration(this, layoutManager.orientation)
+        binding.rvView.addItemDecoration(itemDecoration)
+
+        findRestaurant()
+    }
+
+    private fun findRestaurant() {
+        showLoading(true)
+        val client = ApiConfig.getApiService().getRestaurant(RESTAURANT_ID)
+        client.enqueue(object : retrofit2.Callback<RestaurantResponse> {
+            override fun onResponse(
+                call: Call<RestaurantResponse>,
+                response: Response<RestaurantResponse>
+            ) {
+                showLoading(false)
+                if (response.isSuccessful) {
+                    val responseBody = response.body()
+                    if (responseBody != null) {
+                        setRestaurantData(responseBody.restaurant)
+                        setReviewData(responseBody.restaurant?.customerReviews)
+                    }
+                } else {
+                    Log.e(TAG, "onFailure: ${response.message()}")
+                }
+            }
+            override fun onFailure(call: Call<RestaurantResponse>, t: Throwable) {
+                showLoading(false)
+                Log.e(TAG, "onFailure: ${t.message}")
+            }
+        })
+    }
+
+    private fun setReviewData(customerReviews: List<CustomerReviewsItem?>?) {
+        val listReview = ArrayList<String>()
+        for (review in customerReviews!!){
+            listReview.add(
+                """
+                    ${review?.review}
+                    -${review?.name}
+                """.trimIndent()
+            )
+        }
+        val adapter = ReviewAdapter(listReview)
+        binding.rvView.adapter = adapter
+        binding.edReview.setText("")
+    }
+
+    private fun setRestaurantData(restaurant: Restaurant?) {
+        binding.tvTitle.text = restaurant?.name
+        binding.tvDescription.text = restaurant?.description
+        Glide.with(this@MainActivity)
+            .load("https://restaurant-api.dicoding.dev/images/large/${restaurant?.pictureId}")
+            .into(binding.ivPicture)
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        if (isLoading) {
+            binding.progressbar.visibility = View.VISIBLE
+        } else {
+            binding.progressbar.visibility = View.GONE
+        }
+
     }
 }
+
+
