fab2c95d9b28d9c00fd95d3b3a7436e8bdf2294c
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-10-10 16:07:24 +0700

    add kode ReviewAdapter

--- app/src/main/java/com/gunder/restaurantreview/ReviewAdapter.kt
+++ app/src/main/java/com/gunder/restaurantreview/ReviewAdapter.kt
@@ -1,4 +1,27 @@
 package com.gunder.restaurantreview
 
-class ReviewAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ReviewAdapter(private val listView: List<String>) :
+    RecyclerView.Adapter<ReviewAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val tvItem: TextView = view.findViewById(R.id.tv_item)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReviewAdapter.ViewHolder {
+        ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_view, parent, false))
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        viewHolder.tvItem.text = listView[position]
+    }
+
+    override fun getItemCount(): Int {
+        listView.size
+    }
 }
